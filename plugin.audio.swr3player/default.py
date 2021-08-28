a8298f61a15d909a81cb4203dcfeec3783e9fa68
hjochman <38923044+hjochman@users.noreply.github.com>
2021-08-28 13:29:30 +0200

    streaming url update

--- plugin.audio.swr3player/default.py
+++ plugin.audio.swr3player/default.py
@@ -12,7 +12,11 @@ title = 'SWR3 Video Livestream'
 
 #view-source:https://www.swr3.de/aktuell/live-blog/video-livestream-104.html
 #<script id="playerJSON" type="text/plain">
-stream = 'https://swrswr3vrhls-i.akamaihd.net/hls/live/780818/vrswr3/master.m3u8'
+#stream = 'https://swrswr3vrhls-i.akamaihd.net/hls/live/780818/vrswr3/master.m3u8'
+# 28.Aug 21 https://swrswr3vrhls-i.akamaihd.net/hls/live/780818/vrswr3/master.m3u8?set-segment-duration=responsive
+# https://swrswr3vrhls-i.akamaihd.net/hls/live/780818/vrswr3/master-720p-3628.m3u8
+stream = 'https://swrswr3vrhls-i.akamaihd.net/hls/live/780818/vrswr3/master-720p-3628.m3u8'
+
 xbmcPlayer = xbmc.Player()
 playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
 xbmc.executebuiltin("ActivateWindow(home)")
