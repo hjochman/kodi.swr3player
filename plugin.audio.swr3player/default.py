bbbbc0969922570bde9adff2bf27ba4726ee998b
hjochman <38923044+hjochman@users.noreply.github.com>
2022-01-15 11:00:41 +0100

    Cleanup

--- plugin.audio.swr3player/default.py
+++ plugin.audio.swr3player/default.py
@@ -5,22 +5,25 @@ import os,xbmcgui,xbmc,xbmcaddon
 import requests as req
 import re
 
+
+title = 'SWR3 Video Livestream'
+url = 'https://www.swr3.de/aktuell/live-blog/video-livestream-104.html'
+pDialog = xbmcgui.DialogProgress()
+pDialog.create(title, 'Initializing script...')
+
 addonID = 'plugin.audio.swr3player'
 addon = xbmcaddon.Addon(id = addonID)
 addonPath = addon.getAddonInfo('path')
 icon = xbmc.translatePath( os.path.join( addonPath , 'icon.png' ) )
-title = 'SWR3 Video Livestream'
 
-pDialog = xbmcgui.DialogProgress()
-pDialog.create(title, 'Initializing script...')
-pDialog.update(25, 'Loding web page')
+pDialog.update(25, 'Loding web page',url)
 
-resp = req.get("https://www.swr3.de/aktuell/live-blog/video-livestream-104.html")
+resp = req.get(url)
 m3u = re.search('".*m3u8"', resp.text).group()
 m3u = m3u[1:-1]
 xbmc.log(m3u,xbmc.LOGNOTICE)
 
-pDialog.update(50, 'Loding playlist')
+pDialog.update(50, 'Loding playlist',m3u)
 m3uResp = req.get(m3u)
 xbmc.log(m3uResp.text,xbmc.LOGNOTICE)
 
@@ -37,3 +40,4 @@ playlist.add( stream, listitem )
 pDialog.close()
 xbmc.executebuiltin("ActivateWindow(home)")
 xbmcPlayer.play(playlist)
+
