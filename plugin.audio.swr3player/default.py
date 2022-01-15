67e452ff97b820fa5d42d7a195a8e14cec8867d6
hjochman <38923044+hjochman@users.noreply.github.com>
2022-01-15 10:53:23 +0100

    code optimization, add progress dialog

--- plugin.audio.swr3player/default.py
+++ plugin.audio.swr3player/default.py
@@ -1,33 +1,39 @@
 # -*- coding: utf-8 -*-
 
 
-import os,sys,xbmcplugin,xbmcgui,xbmc,xbmcaddon
+import os,xbmcgui,xbmc,xbmcaddon
 import requests as req
 import re
 
 addonID = 'plugin.audio.swr3player'
 addon = xbmcaddon.Addon(id = addonID)
 addonPath = addon.getAddonInfo('path')
-profilePath = addon.getAddonInfo('profile')
 icon = xbmc.translatePath( os.path.join( addonPath , 'icon.png' ) )
 title = 'SWR3 Video Livestream'
+
+pDialog = xbmcgui.DialogProgress()
+pDialog.create(title, 'Initializing script...')
+pDialog.update(25, 'Loding web page')
 
 resp = req.get("https://www.swr3.de/aktuell/live-blog/video-livestream-104.html")
 m3u = re.search('".*m3u8"', resp.text).group()
 m3u = m3u[1:-1]
 xbmc.log(m3u,xbmc.LOGNOTICE)
 
+pDialog.update(50, 'Loding playlist')
 m3uResp = req.get(m3u)
 xbmc.log(m3uResp.text,xbmc.LOGNOTICE)
 
+pDialog.update(75, 'Extracting video URL')
 stream = re.search('.*master-720.*\n', m3uResp.text).group().strip()
 xbmc.log(stream,xbmc.LOGNOTICE)
 
+pDialog.update(100, 'Starting playback')
 xbmcPlayer = xbmc.Player()
 playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-xbmc.executebuiltin("ActivateWindow(home)")
 listitem = xbmcgui.ListItem( title, iconImage=icon, thumbnailImage=icon)
 playlist.clear()
 playlist.add( stream, listitem )
+pDialog.close()
+xbmc.executebuiltin("ActivateWindow(home)")
 xbmcPlayer.play(playlist)
-
