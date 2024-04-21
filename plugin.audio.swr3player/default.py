f3c96639859146c0fe8013c731d0ea47725cd769
hjochman <38923044+hjochman@users.noreply.github.com>
2024-04-21 16:59:31 +0200

    update to kodi nexus

--- plugin.audio.swr3player/default.py
+++ plugin.audio.swr3player/default.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-import os,xbmcgui,xbmc,xbmcaddon
+import os,xbmcgui,xbmc,xbmcaddon,xbmcvfs
 import requests as req
 import re
-
 
 title = 'SWR3 Video Livestream'
 url = 'https://www.swr3.de/aktuell/live-blog/video-livestream-104.html'
@@ -14,27 +13,30 @@ pDialog.create(title, 'Initializing script...')
 addonID = 'plugin.audio.swr3player'
 addon = xbmcaddon.Addon(id = addonID)
 addonPath = addon.getAddonInfo('path')
-icon = xbmc.translatePath( os.path.join( addonPath , 'icon.png' ) )
+icon = xbmcvfs.translatePath( os.path.join( addonPath , 'icon.png' ) )
 
-pDialog.update(25, 'Loding web page',url)
+xbmc.log(url,xbmc.LOGINFO)
+pDialog.update(25, 'Loding web page')
 
 resp = req.get(url)
 m3u = re.search('".*m3u8"', resp.text).group()
 m3u = m3u[1:-1]
-xbmc.log(m3u,xbmc.LOGNOTICE)
+xbmc.log(m3u,xbmc.LOGINFO)
 
-pDialog.update(50, 'Loding playlist',m3u)
+pDialog.update(50, 'Loding playlist')
 m3uResp = req.get(m3u)
-xbmc.log(m3uResp.text,xbmc.LOGNOTICE)
+xbmc.log(m3uResp.text,xbmc.LOGINFO)
 
 pDialog.update(75, 'Extracting video URL')
 stream = re.search('.*master-720.*\n', m3uResp.text).group().strip()
-xbmc.log(stream,xbmc.LOGNOTICE)
+xbmc.log(stream,xbmc.LOGINFO)
 
 pDialog.update(100, 'Starting playback')
 xbmcPlayer = xbmc.Player()
 playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-listitem = xbmcgui.ListItem( title, iconImage=icon, thumbnailImage=icon)
+listitem = xbmcgui.ListItem( title)
+listart = "{ 'icon':'%s', 'thumb': '%s' }" % (icon, icon)
+listitem.setArt( listart )
 playlist.clear()
 playlist.add( stream, listitem )
 pDialog.close()
